c61396ccb8cec0fbe5fff7bc4ce8af2357f2740c
nyxflower <hxu31@sheffield.ac.uk>
2019-11-12 10:10:51 -0500

    setup

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import torch
+import pickle
+
+
+# ############## io #################
+def save_pickle(obj, path):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f)
+
+
+def load_pickle(path):
+    with open(path, 'rb') as f:
+        obj = pickle.load(f)
+    return obj
+
+
+
