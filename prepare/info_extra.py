f323ce34e117900becf336e216bdff10bf90b00a
nyxflower <nyx0flower@gmail.com>
2020-02-13 19:54:16 -0500

    Process error case

--- prepare/info_extra.py
+++ prepare/info_extra.py
@@ -18,18 +18,17 @@ columns = data.columns.to_list()
 data = data.to_numpy()
 
 # ############# process #############
-start = int(sys.argv[1])
-try:
-    end = int(sys.argv[2])
-except IndexError:
-    end = data.shape[0]
-
-# init out
-pos_data, neg_data = OrderedDict(), OrderedDict()
+# with open('./prepare/extracted_data/err_idx_list.pkl', 'br') as f:
+#     err_list = pickle.load(f)
 err_list = []
+with open('./prepare/extracted_data/pos_data.pkl', 'br') as f:
+    pos_data = pickle.load(f)
+with open('./prepare/extracted_data/neg_data.pkl', 'br') as f:
+    neg_data = pickle.load(f)
 # key: pdb;
 # value: {aa_seq: ndarray(int8), at_seq: ndarray(int8), coo: ndarray(float16)}
-
+end = data.shape[0]
+start = 0
 with tqdm(total=end - start, file=sys.stdout) as pbar:
     for idx in range(start, end):
         # init
@@ -38,31 +37,45 @@ with tqdm(total=end - start, file=sys.stdout) as pbar:
         # load pdb data
         try:
             with open('../PS_jj/data/ppp/{}.pdb'.format(pdb), 'r') as f:
-                pdb_data = f.read().replace('\n', '')
+                pdb_data = f.read().replace('\n', ',')
+            # match_obj = [i for i in re.finditer(' TER ', pdb_data)]
+            # if len(match_obj) > 0:
+            #     pdb_data = pdb_data[:match_obj[-1].span()[0]]
         except FileNotFoundError:
             pbar.set_description('processed: %d' % (1 + idx))
             pbar.update(1)
             continue
         # process text data
         try:
+            # arrive at ATOM section
             match_obj = re.search('ATOM {4}[ \d][ \d]\d', pdb_data)
-            pdb_data = pdb_data[match_obj.span()[0]:]
-            match_obj = re.search('TER', pdb_data)
-            pdb_data = pdb_data[:match_obj.span()[0]].replace('0100.', '0 100.').split()
-            pdb_data = np.array(pdb_data).reshape((-1, 12))
+            pdb_data = pdb_data[match_obj.span()[0]:].split(',')
 
-            aa_seq = np.array([aa_sym_to_idx[i] for i in pdb_data[:, 3]],
-                              dtype=np.int8)
-            coo = pdb_data[:, [6, 7, 8]].astype(np.float16)
-            at_seq = []
-            for i in pdb_data[:, 11]:
-                try:
-                    at_seq.append(at_sym_to_idx[i])
-                except KeyError:
-                    j = len(at_sym_to_idx)
-                    at_sym_to_idx[i] = j
-                    at_seq.append(j)
+            # atom sequence and residue sequence
+            aa_seq, at_seq = [], []
+            for i in pdb_data:
+                if i[:4] == 'ATOM':
+                    m = i[76:78].replace(' ', '')
+                    n = i[17:20].replace(' ', '')
+                    try:
+                        at_seq.append(at_sym_to_idx[m])
+                    except KeyError:
+                        j = len(at_sym_to_idx)
+                        at_sym_to_idx[m] = j
+                        at_seq.append(j)
+                    try:
+                        aa_seq.append(aa_sym_to_idx[n])
+                    except KeyError:
+                        k = len(aa_sym_to_idx)
+                        aa_sym_to_idx[n] = k
+                        aa_seq.append(k)
             at_seq = np.array(at_seq, dtype=np.int8)
+            aa_seq = np.array(aa_seq, dtype=np.int8)
+
+            # coordinate matrix - x y z
+            coo = np.array([[float(i[30:38]), float(i[38:46]), float(i[46:54])]
+                            for i in pdb_data if i[:4] == 'ATOM'],
+                           dtype=np.float16)
 
             out = {'aa_seq': aa_seq, 'at_seq': at_seq, 'coo': coo}
 
@@ -92,29 +105,4 @@ with open('./prepare/extracted_data/neg_data.pkl', 'bw') as f:
 with open('./prepare/extracted_data/err_idx_list.pkl', 'bw') as f:
     pickle.dump(err_list, f)
 print('skip {} samples'.format(len(err_list)))
-#
-# pdb = '3DKW'
-# with open('../PS_jj/data/ppp/3DKW.pdb', 'r') as f:
-#     pdb_data = f.read().replace('\n', '')
-#
-# match_obj = re.search('ATOM {4}[ \d][ \d]\d', pdb_data)
-# if not match_obj:
-#     pass  # //TODO
-# pdb_data = pdb_data[match_obj.span()[0]:]
-# match_obj = re.search('TER', pdb_data)
-# pdb_data = pdb_data[:match_obj.span()[0]].replace('0100.', '0 100.').split()
-# pdb_data = np.array(pdb_data).reshape((-1, 12))
-#
-# aa_seq = np.array([aa_sym_to_idx[i] for i in pdb_data[:, 3]], dtype=np.int8)
-# coo = pdb_data[:, [6, 7, 8]].astype(np.float16)
-#
-# # //TODO: save and try
-# at_seq = []
-# for i in pdb_data[:, 11]:
-#     try:
-#         at_seq.append(at_sym_to_idx[i])
-#     except KeyError:
-#         j = len(at_sym_to_idx)
-#         at_sym_to_idx[i] = j
-#         at_seq.append(j)
-# at_seq = np.array(at_seq, dtype=np.int8)
+
