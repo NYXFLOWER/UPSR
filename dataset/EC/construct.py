6041e09ef88a045e90c4c044c9ed564f8a22f29e
nyxflower <nyx0flower@gmail.com>
2021-03-18 01:43:08 -0400

    add se3transformer

--- dataset/EC/construct.py
+++ dataset/EC/construct.py
@@ -1,11 +1,34 @@
-from py3pdb.download import download_pdb
+#%%
 import os
 import pickle
+import requests
+
+def download_pdb(pdb_id, outfile):
+    page = 'http://files.rcsb.org/view/{}.pdb'.format(pdb_id)
+    req = requests.get(page)
+    if req.status_code == 200:
+        response = req.text
+        if outfile:
+            with open(outfile, 'w') as f:
+                f.write(response)
+        return 1
+    else:
+        return 0
+
 
 ec3 = [3, 4, 24]
-in_dir = 'ec_data/ec_{}/ec_{}_{}'.format(ec3[0], ec3[0], ec3[1])
 in_name = 'ec_pdb_{}_{}_{}.pkl'.format(ec3[0], ec3[1], ec3[2])
 
-with open(os.path.join(in_dir, in_name)) as file:
+with open(os.path.join(in_name), 'rb') as file:
     ec_pdb = pickle.load(file)
 
+
+# %%
+from Bio.PDB import PDBParser
+
+parser = PDBParser()
+if download_pdb('1nqd', '1nqd.pdb'):
+    structure = parser.get_structure('aaaaa1', '1nqd.pdb')
+    print(structure)
+
+# %%
--- main.py
+++
@@ -1,4 +0,0 @@
-from src.utils import load_dataset
-
-pos_data, neg_data = load_dataset('.', 2, 20)
-
